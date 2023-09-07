deefa163e96f4a12fe09874f0cd550fefff0880f
EmilNilsson00 <emilnilsson00@icloud.com>
2023-09-07 16:04:14 +0200

    Created Staff and Employee classes

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Employee extends Staff {
+
+    public Employee(String name, String gender, int id, String employment) {
+        super(name, gender, id, employment);
+    }
+}
---
+++ src/main/java/org/example/Staff.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Staff {
+
+    private String name;
+    private String gender;
+    private int id;
+    private String employment;
+
+    public Staff(String name, String gender, int id, String employment) {
+        this.name = name;
+        this.gender = gender;
+        this.id = id;
+        this.employment = employment;
+    }
+
+}
